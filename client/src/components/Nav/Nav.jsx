b9fd34605491e92455072e27c844817cae8024f9
Ozodbek16 <progamer16.06.07@gmail.com>
2022-10-20 18:11:55 +0500

    feat: container; fix: nav;

---
+++ client/src/components/Nav/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './Nav.module.css'
+
+const Nav = () => {
+    return (
+        <nav className='container'>
+            <div className={classes.box}></div>
+        </nav>
+    )
+}
+
+export default Nav
--- client/src/layouts/no-user/No_user.jsx
+++ client/src/layouts/no-user/No_user.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Nav from '../../components/Nav/Nav'
 
 const Nouser = (props) => {
     return (
         <>
+            <Nav />
             <div className="body">
                 {props.children}
             </div>
