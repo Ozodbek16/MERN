59ac1e2ec05fd7541c5c9b1d06541cfffe868dbc
Ozodbek16 <progamer16.06.07@gmail.com>
2022-10-22 21:04:46 +0500

    feat: nav fixed and sign-in page created

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Home from './containers/home/Home'
+import SignIn from './containers/SignIn/SignIn';
 import Nouser from './layouts/no-user/No_user'
 import { Routes, Route } from 'react-router-dom'
 
@@ -9,6 +10,7 @@ function App() {
       <Nouser>
         <Routes>
           <Route path={'/'} element={<Home />} />
+          <Route path={'/sign-in'} element={<SignIn />} />
         </Routes>
       </Nouser>
     </div>
