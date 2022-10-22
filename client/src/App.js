4732317224cd79029cd2ac4dc7bff80c7636e1d4
Ozodbek16 <progamer16.06.07@gmail.com>
2022-10-22 20:59:32 +0500

    feat: react-router-dom added

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Home from './containers/home/Home'
 import Nouser from './layouts/no-user/No_user'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <Nouser>
-        <Home />
+        <Routes>
+          <Route path={'/'} element={<Home />} />
+        </Routes>
       </Nouser>
     </div>
   );
--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import classes from './Nav.module.css'
 
 const Nav = () => {
     return (
-        <nav className='container'>
+        <nav className={classes.nav}>
+            <div className="container">
+                <div className={classes.row}>
+                    <div className={classes.logo}>
+                        <Link to={"/"}>Linklarim</Link>
+                    </div>
+                    <div className={classes.signBox}>
+                        <Link className={classes.in} to={"/sign-in"}>Sign-in</Link>
+                        <Link className={classes.up} to={"/sign-up"}>Sign-up</Link>
+                    </div>
+                </div>
+            </div>
         </nav>
     )
 }
